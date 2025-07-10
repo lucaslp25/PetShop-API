b38adee438e3b82e0f0f55022af30b0195cad45e
Lucas Lopes <lp.skt25@gmail.com>
2025-07-09 23:52:31 -0300

    Project created

---
+++ src/test/java/com/lucasdev/petshop_api/PetShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdev.petshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
